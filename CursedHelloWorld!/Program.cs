590bb592794e49124d2628a953d5827b6f9a31f4
PhenomenalMoss <147830809+PhenomenalMoss@users.noreply.github.com>
2023-10-26 20:46:07 +1000

    CursedHelloWorld!
    Just want to make it as dumb as possible.

---
+++ CursedHelloWorld!/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+namespace CursedHelloWorld
+{
+    class Output
+    {
+        public string helloworld { get; set; }
+        protected int i = -1;
+        protected int I
+        {
+            get 
+            {
+                i++;
+                return i; 
+            }
+            set
+            {
+                i = value;
+            }
+        }
+        public virtual void Write() 
+        {
+            Console.WriteLine();
+        }
+    }
+    class H : Output
+    {
+        public override void Write()
+        {
+            Console.Write(helloworld[I]);
+        }
+    }
+    class HE : H
+    {
+        public override void Write()
+        {
+            base.Write();
+            Console.Write(helloworld[I]);
+        }
+    }
+    class HEL : HE
+    {
+        public override void Write()
+        {
+            base.Write();
+            Console.Write(helloworld[I]);
+        }
+    }
+    class HELL : HEL
+    {
+        public override void Write()
+        {
+            base.Write();
+            Console.Write(helloworld[I]);
+        }
+    }
+    class HELLO : HELL
+    {
+        public override void Write()
+        {
+            base.Write();
+            Console.Write(helloworld[I]);
+        }
+    }
+    class HELLO_ : HELLO
+    {
+        public override void Write()
+        {
+            base.Write();
+            Console.Write(helloworld[I]);
+        }
+    }
+    class HELLO_W : HELLO_
+    {
+        public override void Write()
+        {
+            base.Write();
+            Console.Write(helloworld[I]);
+        }
+    }
+    class HELLO_WO : HELLO_W
+    {
+        public override void Write()
+        {
+            base.Write();
+            Console.Write(helloworld[I]);
+        }
+    }
+    class HELLO_WOR : HELLO_WO
+    {
+        public override void Write()
+        {
+            base.Write();
+            Console.Write(helloworld[I]);
+        }
+    }
+    class HELLO_WORL : HELLO_WOR
+    {
+        public override void Write()
+        {
+            base.Write();
+            Console.Write(helloworld[I]);
+        }
+    }
+    class HELLO_WORLD : HELLO_WORL
+    {
+        public override void Write()
+        {
+            base.Write();
+            Console.Write(helloworld[I]);
+        }
+    }
+    class HELLO_WORLDI : HELLO_WORLD
+    {
+        public override void Write()
+        {
+            base.Write();
+            Console.Write(helloworld[I]);
+        }
+    }
+    class HELLO_WORLDINEXT : HELLO_WORLDI
+    {
+        public override void Write()
+        {
+            base.Write();
+            Console.WriteLine();
+        }
+    }
+    class CursedHelloworld
+    {
+        public static void Main()
+        {
+            HELLO_WORLDINEXT hw = new HELLO_WORLDINEXT();
+            hw.helloworld = "Hello World!";
+            hw.Write();
+        }
+    }
+}
